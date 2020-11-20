c1ef9edf54b5df0a819d87986b0918b94c883ee1
gtxtreme <thakkargaurav409@gmail.com>
2020-11-21 00:26:42 +0530

    Just little meaningful organisation

--- app/src/main/java/com/example/vocalforlocal/Login.java
+++ app/src/main/java/com/example/vocalforlocal/Login.java
@@ -13,6 +13,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.vocalforlocal.categories.Categories;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
