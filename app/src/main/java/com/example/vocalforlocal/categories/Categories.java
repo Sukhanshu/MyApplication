d6388e3ebd0ec717074bc1fea51f4de634bb3353
gtxtreme <thakkargaurav409@gmail.com>
2020-11-21 00:27:11 +0530

    Include reference to Db

--- app/src/main/java/com/example/vocalforlocal/categories/Categories.java
+++ app/src/main/java/com/example/vocalforlocal/categories/Categories.java
@@ -1,4 +1,4 @@
-package com.example.vocalforlocal;
+package com.example.vocalforlocal.categories;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,15 +8,20 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.vocalforlocal.R;
 import com.example.vocalforlocal.categories.DigiArt;
 import com.example.vocalforlocal.categories.HomeDecor;
 import com.example.vocalforlocal.categories.Paintings;
 import com.example.vocalforlocal.categories.Sketches;
+import com.example.vocalforlocal.db.TinyDb;
+import com.example.vocalforlocal.models.Product;
+
+import java.util.ArrayList;
 
 public class Categories extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "Categories";
-    private Button btnPaintings, btnSketches, btnDigiArt, btnHomeDecor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +31,15 @@ public class Categories extends AppCompatActivity implements View.OnClickListene
 
     @Override
     public void onClick(View v) {
+        TinyDb tinyDb = new TinyDb(getApplication());
+        ArrayList<Product> products = new ArrayList<>();
+        tinyDb.putListObject("cart_products",products);
         switch (v.getId()){
             case R.id.btnDigiArt:
                 Log.d(TAG,"Digital Art");
                 startActivity(new Intent(this, DigiArt.class));
                 break;
             case R.id.btnHomeDecor:
-
                 startActivity(new Intent(this, HomeDecor.class));
                 break;
             case R.id.btnPaintings:
